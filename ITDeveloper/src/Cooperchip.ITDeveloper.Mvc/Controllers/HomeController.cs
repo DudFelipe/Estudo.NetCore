e6a74fca4723ba7378a9adbaee2afb3ae229d1a2
Eduardo Serra <efelipe10@hotmail.com>
2020-06-09 13:09:08 -0300

    Criando a página de icones para alguns testes

--- ITDeveloper/src/Cooperchip.ITDeveloper.Mvc/Controllers/HomeController.cs
+++ ITDeveloper/src/Cooperchip.ITDeveloper.Mvc/Controllers/HomeController.cs
@@ -28,6 +28,11 @@ namespace Cooperchip.ITDeveloper.Mvc.Controllers
             return View();
         }
 
+        public IActionResult Icones()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
