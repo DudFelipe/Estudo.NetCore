30f30ef6bab74b8b4c2f6a2febff0089a9abc950
Eduardo Serra <efelipe10@hotmail.com>
2020-06-23 18:44:32 -0300

    Tag helper um pouco mais avancada

--- ITDeveloper/src/Cooperchip.ITDeveloper.Domain/Models/Paciente.cs
+++ ITDeveloper/src/Cooperchip.ITDeveloper.Domain/Models/Paciente.cs
@@ -2,6 +2,7 @@
 using Cooperchip.ITDeveloper.Domain.Enums;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
@@ -21,6 +22,7 @@ namespace Cooperchip.ITDeveloper.Domain.Models
         public Guid EstadoPacienteId { get; set; }
         public virtual EstadoPaciente EstadoPaciente { get; set; }
 
+        [DisplayName(displayName: "Nome do Paciente")]
         public string Nome { get; set; }
         public DateTime DataNascimento { get; set; }
         public DateTime DataInternacao { get; set; }
